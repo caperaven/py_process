1c6691657fa9ec4f01d7da8b683993e68d24d773
caperaven <caperaven@gmail.com>
2023-08-15 20:17:35 +0200

    update loop example in notebook and look for step in process it not defined

--- src/action_systems/loop_actions.py
+++ src/action_systems/loop_actions.py
@@ -13,5 +13,8 @@ class Default:
 
         process_step = await get_value(args.get("process_step"), context, process, item)
 
+        if isinstance(collection, str):
+            process_step = process["steps"][process_step]
+
         for i in collection:
             await api.run(process_step, context, process, i)
