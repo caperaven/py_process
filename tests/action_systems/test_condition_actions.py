611540aeb39a2a6fc6008d58b9c2d0645be70b8b
caperaven <caperaven@gmail.com>
2023-08-22 21:41:21 +0200

    add switch actions

--- tests/action_systems/test_condition_actions.py
+++ tests/action_systems/test_condition_actions.py
@@ -56,3 +56,38 @@ async def test_condition_fail(monkeypatch):
 
     await ConditionActions.perform(step, context, None, None)
     assert printed_message == "fail"
+
+
+@pytest.mark.asyncio
+async def test_condition_process_step(monkeypatch):
+    printed_message = None
+
+    def mock_print(x):
+        nonlocal printed_message
+        printed_message = x
+
+    monkeypatch.setattr('builtins.print', mock_print)
+
+    context = {
+        "firstName": "John"
+    }
+
+    run_step = {
+        "args": {
+            "condition": "$c{firstName} == 'John'",
+        },
+        "pass_step": "pass"
+    }
+
+    process = {
+        "steps": {
+            "pass": {
+                "type": "console",
+                "action": "print",
+                "args": {"message": "pass"}
+            }
+        }
+    }
+
+    await ConditionActions.perform(run_step, context, process, None)
+    assert printed_message == "pass"
